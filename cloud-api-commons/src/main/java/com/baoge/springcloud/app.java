4913c141e0c62028ff0540c402a144cf8990827b
shaoxubao <shaoxubao-sz@fangdd.com>
2020-05-16 23:44:04 +0800

    重构模块

---
+++ cloud-api-commons/src/main/java/com/baoge/springcloud/app.java
@@ -0,0 +1,9 @@
+package com.baoge.springcloud;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <xubao_shao@163.com>
+ * Date:   2020/5/16
+ */
+public class app {
+}
