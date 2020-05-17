1a1215ad92d86979fc612b428a9d8e2149ccee88
shaoxubao <shaoxubao-sz@fangdd.com>
2020-05-18 00:07:14 +0800

    Eureka 集群

---
+++ cloud-eureka-server7002/src/main/java/com/baoge/springcloud/EurekaMain7002.java
@@ -0,0 +1,21 @@
+package com.baoge.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Copyright 2018-2028 Baoge All Rights Reserved.
+ * Author: Shao Xu Bao <xubao_shao@163.com>
+ * Date:   2020/5/18
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+
+}
